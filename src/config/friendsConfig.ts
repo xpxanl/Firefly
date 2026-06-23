8b24f874adca8ebe1edfd89af01d8bfd827d6b2e
xpxanl <myp370246@gmail.com>
2026-06-23 12:42:35 +0800

    mz

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -5,10 +5,46 @@ import type { FriendLink, FriendsPageConfig } from "../types/friendsConfig";
 // 友链页面配置
 export const friendsPageConfig: FriendsPageConfig = {
 	// 页面标题，如果留空则使用 i18n 中的翻译
-	title: "",
+	title: "免责声明",
 
 	// 页面描述文本，如果留空则使用 i18n 中的翻译
-	description: "",
+	description: "Like please buy genuine and authorize legal use
+
+This is only suitable for testing
+
+Reprinted from major media and networks
+
+User agreement:
+
+Please do not run and use it before reading and accepting all the terms of this license agreement. Only when the user agrees to the license terms will he have the right to use the test. Using this test means that the user accepts the license terms.
+
+1. This is for testing and research purposes only, and the copyright belongs to the issuing company. No organization or individual may disseminate or use it for any commercial purposes, otherwise all consequences will be borne by the organization and individual! Our company will not bear any legal and joint liability.
+2. No responsibility is assumed for any adverse effects caused by using this software.
+3. Please consciously delete it within 24 hours after downloading. If you like this game, please buy the genuine one and authorize it for legal use.
+
+Please read this description carefully before using the software. Once you start running and using it, you will agree to all the contents of the statement by default, and will not be responsible for any adverse effects!
+
+喜欢请购买正版并授权合法使用
+
+此只适合测试试用版本
+
+转载自各大媒体和网络
+
+用户使用协议：
+
+在阅读并接受本授权协议所有的条款之前，请勿运行和使用。在用户同意此授权条款时，才具有使用测试的权限。使用此测试即表示用户接受此授权条款。
+
+1．此仅供爱好者测试及研究之用,版权归发行公司所有。任何组织或个人不得传播或用于任何商业用途,否则一切后果由该组织及个人承担! 我方将不承担任何法律及连带责任。
+2．对使用本本测试版本后产生的任何不良影响,我方不承担任何法律及连带责任。
+3．请自觉与下载后２４小时内删除．如果喜欢本游戏，请购买正版并授权合法使用。
+
+警告：在使用该软件(文件）之前请先仔细阅读本说明
+这个游戏包含面向18岁及以上的成人性取向内容如果您未满18岁，如果成人内容冒犯了您，
+或者你正在从法律禁止成人内容的任何国家或地区访问本游戏，请立即离开！本游戏中的性行为是幻想，不是现实，
+不提倡你按照它的内容中重演任何事情游戏中所有的人物都至少年满18周岁.如果您理解并接受这些条款，您可以进入
+一旦开始运行和使用以后就默认同意了该申明中的所有内容，若有任何不良影响概不负责！
+
+(注：此申明包括分享的，视频，游戏，漫画，软件，）",
 
 	// 是否显示底部自定义内容（friends.mdx 中的内容）
 	showCustomContent: true,
@@ -20,45 +56,3 @@ export const friendsPageConfig: FriendsPageConfig = {
 	randomizeSort: false,
 };
 
-// 友链配置
-export const friendsConfig: FriendLink[] = [
-	{
-		title: "夏夜流萤",
-		imgurl:
-			"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
-		desc: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
-		siteurl: "https://blog.cuteleaf.cn",
-		tags: ["Blog"],
-		weight: 10, // 权重，数字越大排序越靠前
-		enabled: true, // 是否启用
-	},
-	{
-		title: "awacg Docs",
-		imgurl: "https://docs-awacg.cuteleaf.cn/logo.png",
-		desc: "Firefly主题模板文档",
-		siteurl: "https://docs-awacg.cuteleaf.cn",
-		tags: ["Docs"],
-		weight: 9,
-		enabled: true,
-	},
-	{
-		title: "Astro",
-		imgurl: "https://avatars.githubusercontent.com/u/44914786?v=4&s=640",
-		desc: "The web framework for content-driven websites. ⭐️ Star to support our work!",
-		siteurl: "https://github.com/withastro/astro",
-		tags: ["Framework"],
-		weight: 8,
-		enabled: true,
-	},
-];
-
-// 获取启用的友链并进行排序
-export const getEnabledFriends = (): FriendLink[] => {
-	const friends = friendsConfig.filter((friend) => friend.enabled);
-
-	if (friendsPageConfig.randomizeSort) {
-		return friends.sort(() => Math.random() - 0.5);
-	}
-
-	return friends.sort((a, b) => b.weight - a.weight);
-};
