3cd27f30d37b1a4734247660f81b0a7a6d20fe01
xpxanl <myp370246@gmail.com>
2026-06-23 13:26:18 +0800

    0

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -12,7 +12,7 @@ export const profileConfig: ProfileConfig = {
 	name: "awacg.us",
 
 	// 个人签名
-	bio: "Hello",
+	bio: "一个分享免费游戏的网站",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
